0c26862fc148cd8660a2a6d440949ba5267dbab1
Julien <julien@hypecode.com>
2020-11-27 00:42:13 +0100

    wip

--- components/project/project.js
+++ components/project/project.js
@@ -19,16 +19,14 @@ const Project = (projectData, key) => {
         <div className="project-container">
             <div className="img-wrapper" style={{ background: `url('${project.illustrationPath}') no-repeat center`, backgroundSize: project.portrait ? 'contain' : 'cover' }}>
             </div>
-            <div className="content-wrapper">
-                <div className="desc-wrapper" {...bind} >
-                    <h2 className="titre">{project.projectName}</h2>
-                    <span className="description">{project.description}</span>
-                    <animated.div className="tags-wrapper" style={{ top: props.top }}>
-                        {project.tags.map((tag, i) => {
-                            return <animated.div className="tag" key={tag + i} style={{ opacity: props.top.interpolate([0, bounds.height], [0, 1]) }}>{tag}</animated.div>
-                        })}
-                    </animated.div>
-                </div>
+            <div className="desc-wrapper" {...bind} >
+                <h2 className="titre">{project.projectName}</h2>
+                <span className="description">{project.description}</span>
+                <animated.div className="tags-wrapper" style={{ top: props.top }}>
+                    {project.tags.map((tag, i) => {
+                        return <animated.div className="tag" key={tag + i} style={{ opacity: props.top.interpolate([0, bounds.height], [0, 1]) }}>{tag}</animated.div>
+                    })}
+                </animated.div>
             </div>
         </div>
     );
